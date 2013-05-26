158bb2646effd84e5efd81965fb5cb4de3cc9058
Florian Oswald <florian.oswald@gmail.com>
2013-05-26 19:33:07 +0100

    initial commit

---
+++ src/umod.cpp
@@ -0,0 +1,182 @@
+
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+
+#include "ufuns.h"
+
+using namespace Rcpp;
+
+//' C++ module for computation of V = U + b*EV
+//'
+//' computes value functions for \code{m} discrete labor supply choices. no time separability assumed, i.e. labor supply is not implied by current resources. computes V on \code{n} states.
+//' @param cashR numeric matrix \code{(n,m)} of cash holdings conditional on labor supply (that's why \code{m} columns)
+//' @param saveR numeric matrix \code{(n,k)} of savings options. k < n.
+//' @param EVR numeric matrix \code{(n,k)} representing expected future value at each state,choice combination
+//' @param hsizeR vector \code{(n,1)}
+//' @param laborR vector \code{(m,1)}, basically \code{seq(from=0,to=1,length=m)}
+//' @param b boolean indicator equal TRUE if you have NAs in savings matrix imposing some borrowing constraint. if b==FALSE, no NA checking is done and results will be incorrect.
+//' @param theta elasticity of substitution between c and h
+//' @param phival value of relative utility difference flat vs house
+//' @param mu weight on additive utility premium
+//' @param gamma coefficient of relative risk aversion
+//' @param cutoff minimum level of consumption. below cutoff, u(c) is quadratically approximated.
+//' @param alpha coefficient on labor
+//' @return list with elements
+//' \item{values}{\code{(n,m)} matrix of conditional value functions. column i is V_i.}
+//' \item{saving}{\code{(n,m)} matrix of conditional savings functions. column i is save_i.}
+//' \item{cons}{\code{(n,m)} matrix of conditional value functions. column i is cons_i.}
+//' \item{dchoiceL}{\code{(n,1)} vector indexing discrete choice at each state.}
+//' \item{maxL}{\code{(n,1)} vector of maximal value. maxL = max_d V_d.}
+//' @author Florian Oswald <florian.oswald@@gmail.com>
+//' @examples
+//' n = 5    # number of states
+//' k = 5    # number of savings choices by state
+//' m = 3    # number of discrete labor choices by state
+//' cash   <- matrix(1:n,n,m)
+//' cash   <- cash + matrix(0:2,n,m,byrow=TRUE)
+//' labo   <- seq(from=0,to=1,length=m)
+//' saving <- matrix(seq(from=0,to=8,length=k),n,k,byrow=TRUE)
+//' EV     <- log(outer(1:n,1:n))
+//' hsize  <- sample(0:2,size=n,replace=TRUE)
+//' pars   <- list(theta=0.2,phival=0.9,mu=0.6,gamma=1.4,cutoff=0.1,alpha=-0.6)
+//' res <- util_module(cashR=cash, saveR=saving, EVR=EV, hsizeR=hsize, laborR=labo, par=pars, b=FALSE)
+//' ##
+//' ## work with borrowing constraint in savings matrix: all saving less than x inadmissible
+//' ##
+//' saving[1,1:3] <-NA	# all savings less than element c(1,4) are illegal in row 1
+//' res <- util_module(cashR=cash, saveR=saving, EVR=EV, hsizeR=hsize, laborR=labo, par=pars, b=FALSE) # WRONG
+//' res <- util_module(cashR=cash, saveR=saving, EVR=EV, hsizeR=hsize, laborR=labo, par=pars, b=TRUE) # RIGHT
+// [[Rcpp::export]]
+List util_module(NumericMatrix cashR, NumericMatrix saveR, NumericMatrix EVR, NumericVector hsizeR, NumericVector laborR, List par, bool b){
+
+    const int n  = cashR.nrow();   // number of states
+	const int m  = cashR.ncol();	// number of discrete choices
+	const int k  = saveR.ncol();   // number of savings choices
+
+	// some input checks
+	if (n != saveR.nrow()){
+		throw Rcpp::exception( "util_module: saveR not equal rows as cashR");
+		return R_NilValue;
+	} 
+	if (n != EVR.nrow()){
+		throw Rcpp::exception( "util_module: EVR not equal rows as cashR");
+		return R_NilValue;
+	} 
+	if (k != EVR.ncol()){
+		throw Rcpp::exception( "util_module: EVR not equal cols as saveR");
+		return R_NilValue;
+	} 
+  if (n != hsizeR.size()){
+		throw Rcpp::exception( "util_module: hsizeR not same number of rows as cashR");
+		return R_NilValue;
+	} 
+  // remark: current structure forces us to have n = k. if that changes in the future, this module can handle it.
+
+	// map to R to arma
+	arma::mat cash(cashR.begin(), n, m, false);	// advanced constructor. no copying.
+	arma::mat EV(EVR.begin(), n, k, false);	
+	arma::mat save(saveR.begin(), n, k, false);	
+	arma::vec labor(laborR.begin(),m,false);
+	arma::vec hsize(hsizeR.begin(),n,false);
+	// Rcpp::List par( par_ ) ;
+
+	// allocate tmpcash, utility, and W matrices
+	arma::mat tmpcash(n,k);
+	arma::mat util(n,k);
+	arma::mat W(n,k);
+
+	// allocate indicator objects if need to subset
+	arma::uvec u(k);
+	arma::rowvec tmpvec2;
+	arma::rowvec tmpvec3(m);	// for discrete choice
+
+	// allocate out objects 
+	arma::mat tmpy(n,m);	// maximal value for each discrete choice (value)
+	arma::mat tmps(n,m);	// optimizing savings choice (value)
+	arma::mat tmpc(n,m);	// implied optimal consumption (value)
+	arma::uvec retiD(n);		// index of optimal discrete choice (index)
+    arma::vec retV(n);		// value of optimal discrete choice 
+  //arma::vec retC(n);		// cons at discrete choice 
+	//arma::vec retS(n);		// save at discrete choice
+  
+
+	// allocate for maximization loop
+	arma::uword j;
+//	arma::uvec::fixed<n> iy;
+	arma::vec y(n);
+	arma::rowvec tmpvec(k);
+
+	
+	// loop over discrete choices
+	for (int i=0;i<m;i++){
+
+		y.zeros();
+
+		// fill tmp with copies of cash
+		tmpcash = repmat(cash.col(i),1,k);
+		// get consumption at each savings choice
+		tmpcash = tmpcash - save;
+		// get utility
+		util    = ufun_discreteL( tmpcash, par, hsize, labor(i) );
+		// get lifetime utility
+		W       = util + EV;
+
+		// maximize
+		// loop over rows of W and find maximal value and it's index for each row.
+		//
+		// insert switch about restricted savings space here
+		// need to construct an index matrix FALSE where is not finite
+		if (b) {
+			// there are NAs in save matrix - need to account for that
+			for (int irow=0;irow<n;irow++){
+			// get current row of savings matrix to see which choices are illegal
+			   tmpvec = save.row(irow);
+			   // for this row, find all infinite indexes
+			   for (int r=0;r<k;r++){
+			       u(r) = arma::is_finite(tmpvec(r));
+			   }
+			   // overwrite with values of W
+			   tmpvec = W.row(irow);
+				// max over row
+			   // while is.na(u[j]), increase j to get to first non-na u[j]
+			   int r = 0;
+			   while ( !u(r) ){
+			   	r += 1;
+			   }
+			   tmpvec2 = tmpvec.subvec(r,k-1);
+			   y(irow) = tmpvec2.max(j);	// j is the uword recording the position of the maximal element
+			   tmpc(irow,i) = tmpcash(irow,j + r);
+			   tmps(irow,i) = save(irow,j + r);	// savings matrix is the same for all discrete labor choices
+			}
+		} else {
+			// there are no NAs in savings matrix, unrestricted choice
+			for (int irow=0; irow<n; irow++) {
+				tmpvec       = W.row(irow);
+				y(irow)      = tmpvec.max( j );
+				tmpc(irow,i) = tmpcash(irow,j);
+				tmps(irow,i) = save(irow,j);	// savings matrix is the same for all discrete labor choices
+		//		iy(irow)    = j + 1; // go to 1 based indices we don't even need those indices anymore.
+			}
+		}
+		// put into return object at discrete choice column i
+		tmpy.col(i)  = y;
+	}
+
+	// find optimal discrete choice
+	for (int irow=0; irow<n; irow++){
+		tmpvec3     = tmpy.row(irow);
+		retV(irow)  = tmpvec3.max( j );
+    //retC(irow)  = tmpc(irow,j);
+    //retS(irow)  = tmps(irow,j);
+		retiD(irow) = j + 1; // record discrete choice as 1-based index.
+	}
+
+  // return vectors at optimal discrete choice: don't return conditional policy functions
+  
+	Rcpp::List list = Rcpp::List::create( _["values"] = tmpy, _["saving"] = tmps, _["cons"] = tmpc, _["dchoiceL"] = retiD, _["maxL"] = retV);
+	return list;
+}
+
---
+++ tests/test1.r
@@ -0,0 +1,118 @@
+# testing util_module
+
+library(testthat)
+library(umod)
+setwd("~/git/umod/tests")
+# setup objects
+
+n = 5    # number of states
+k = 5    # number of savings choices by state
+m = 3    # number of discrete labor choices by state
+
+cash   <- matrix(1:n,n,m)
+cash   <- cash + matrix(0:2,n,m,byrow=TRUE)
+labo   <- seq(from=0,to=1,length=m)
+saving <- matrix(seq(from=0,to=8,length=k),n,k,byrow=TRUE)
+EV     <- log(outer(1:n,1:n))
+hsize  <- sample(0:2,size=n,replace=TRUE)
+pars   <- list(theta=0.2,phival=0.9,mu=0.6,gamma=1.4,cutoff=0.1,alpha=-0.6)
+
+
+res <- util_module(cashR=cash, saveR=saving, EVR=EV, hsizeR=hsize, laborR=labo, par=pars, b=FALSE)
+
+context("testing that C output is sane")
+test_that("res is a list",{
+  expect_that( is.list(res), is_true() )})
+test_that("res$values is a numeric matrix (n,m)",{
+  expect_that( is.matrix(res$values) & all.equal(dim(res$values),c(n,m)) & !any(is.na(res$values)), is_true() )})
+test_that("res$cons is a numeric matrix (n,m)",{
+  expect_that( is.matrix(res$cons) & all.equal(dim(res$cons),c(n,m)) & !any(is.na(res$cons)), is_true() )})
+test_that("res$saving is a numeric matrix (n,m)",{
+  expect_that( is.matrix(res$saving) & all.equal(dim(res$saving),c(n,m)) & !any(is.na(res$saving)), is_true() )})
+test_that("res$dchoiceL is matrix (n,1) of integers",{
+  expect_that( is.matrix(res$dchoiceL) & all.equal(dim(res$dchoiceL),c(n,1)) & !any(is.na(res$dchoiceL)), is_true() )})
+test_that("res$maxL is matrix (n,1) of doubles",{
+  expect_that( is.matrix(res$maxL) & all.equal(dim(res$maxL),c(n,1)) & !any(is.na(res$maxL)), is_true() )})
+
+
+## test with some NA values
+## ========================
+
+saving2 <- saving
+saving2[1,1:3] <- NA
+saving2[2,1:2] <- NA
+saving2[3,1] <- NA
+
+res2 <- util_module(cashR=cash, saveR=saving2, EVR=EV, hsizeR=hsize, laborR=labo, par=pars, b=TRUE)
+
+context("testing that C output is sane with NAs")
+test_that("res2 is a list",{
+  expect_that( is.list(res2), is_true() )})
+test_that("res2$values is a numeric matrix (n,m)",{
+  expect_that( is.matrix(res2$values) & all.equal(dim(res2$values),c(n,m)) & !any(is.na(res2$values)), is_true() )})
+test_that("res2$cons is a numeric matrix (n,m)",{
+  expect_that( is.matrix(res2$cons) & all.equal(dim(res2$cons),c(n,m)) & !any(is.na(res2$cons)), is_true() )})
+test_that("res2$saving is a numeric matrix (n,m)",{
+  expect_that( is.matrix(res2$saving) & all.equal(dim(res2$saving),c(n,m)) & !any(is.na(res2$saving)), is_true() )})
+test_that("res2$dchoiceL is matrix (n,1) of integers",{
+  expect_that( is.matrix(res2$dchoiceL) & all.equal(dim(res2$dchoiceL),c(n,1)) & !any(is.na(res2$dchoiceL)), is_true() )})
+test_that("res2$maxL is matrix (n,1) of doubles",{
+  expect_that( is.matrix(res2$maxL) & all.equal(dim(res2$maxL),c(n,1)) & !any(is.na(res2$maxL)), is_true() )})
+
+
+
+# do same calculations in R
+
+source("rinline.r")  # get R utility function
+
+rres <- matrix(NA,n,m)
+rcons <- rres
+rsave <- rres
+
+
+for (i in 1:m){
+  tmp <- cash[,i]-saving
+  
+  util <- ufun_labouR_disc(e=tmp,s=hsize,l=labo[i],params=pars)
+  W <- util + EV
+  rres[,i] <- apply(W,1,max)
+  rsave[,i] <- saving[cbind(1:n,apply(W,1,which.max))]
+  rcons[,i] <- tmp[cbind(1:n,apply(W,1,which.max))]
+}
+dchoiceL <- matrix(apply(rres,1,which.max),n,1)
+#rsave <- matrix(rsave[cbind(1:n,dchoiceL)],n,1)
+#rcons <- matrix(rcons[cbind(1:n,dchoiceL)],n,1)
+
+
+Rres <- list(values=rres,saving=rsave,cons=rcons,dchoiceL=dchoiceL,maxL=matrix(apply(rres,1,max),n,1))
+
+context("testing that C output is equal to R")
+test_that("R and C output are identical",{
+  expect_that( all.equal(Rres, res),is_true()) })
+
+
+# with NA values
+# R function can't handle NAs
+
+# for (i in 1:m){
+#   tmp <- cash[,i]-saving2
+#   
+#   util <- ufun_labouR_disc(e=tmp,s=hsize,l=labo[i],params=pars)
+#   W <- util + EV
+#   rres[,i] <- apply(W,1,max)
+#   rsave[,i] <- saving[cbind(1:n,apply(W,1,which.max))]
+#   rcons[,i] <- tmp[cbind(1:n,apply(W,1,which.max))]
+# }
+# dchoiceL <- matrix(apply(rres,1,which.max),n,1)
+#rsave <- matrix(rsave[cbind(1:n,dchoiceL)],n,1)
+#rcons <- matrix(rcons[cbind(1:n,dchoiceL)],n,1)
+
+
+# Rres <- list(values=rres,saving=rsave,cons=rcons,dchoiceL=dchoiceL,maxL=matrix(apply(rres,1,max),n,1))
+
+# context("testing that C output is equal to R with NAs")
+# test_that("R and C output are identical",{
+#   expect_that( all.equal(Rres, res2),is_true()) })
+# 
+
+

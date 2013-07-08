5d12a2aec335a524bf1cba5df6e9c65cf31d29ac
Florian Oswald <florian.oswald@gmail.com>
2013-07-08 20:52:18 +0100

    back to normal

--- src/ufuns.h
+++ src/ufuns.h
@@ -29,8 +29,8 @@ inline void handler(int sig) {
 
   // print out all the frames to stderr
   fprintf(stderr, "Error: signal %d:\n", sig);
-  //backtrace_symbols_fd(array, size, STDERR_FILENO);
-  backtrace_symbols(array, size);
+  backtrace_symbols_fd(array, size, STDERR_FILE);
+  //backtrace_symbols(array, size);
   exit(1);
 }
 
